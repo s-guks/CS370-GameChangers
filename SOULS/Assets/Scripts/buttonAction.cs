3bef1a8ef0d8a6f00c182ce2b79fbbff37b645d2
dlinbeck <142837109+dlinbeck@users.noreply.github.com>
2023-10-16 21:03:05 -0400

    Added clickable button (model and scripts)

---
+++ SOULS/Assets/Scripts/buttonAction.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonAction : MonoBehaviour
+{
+    public void buttonMethod() {
+        Debug.Log("Button Clicked!"); //temporary notice
+        // Future button animation goes here
+        /* Then call to other functions here like:
+            -End Turn
+            -Draw Card  */
+    }
+}
